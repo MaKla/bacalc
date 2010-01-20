7fa022139d960bf23298c88afaadf0f76fd53ec6
MaKla <martin.klapprot@makrotan.de>
2010-01-20 13:00:25 +0100

    added some debug code to server

--- src/v4/server2.cpp
+++ src/v4/server2.cpp
@@ -3,13 +3,21 @@
 #include <arpa/inet.h>
 #include <string.h>
 #include <unistd.h>
+#include <iostream>
 
 const char* address = "127.0.0.1";
 const int port = 9735;
 
+using namespace std;
+
 int main() {
 
+	cout << "Initializing socket" << endl;
     int s = socket(AF_INET, SOCK_STREAM, 0);
+    if (s == -1) {
+		perror("Error on initializing socket");
+		exit(1);
+	}
 
     struct sockaddr_in sa;
 
@@ -19,17 +27,34 @@ int main() {
 
     int sas = sizeof(sa);
 
-    bind(s, (struct sockaddr*) &sa, sas);
+
+    cout << "Binding on ip " << address << " port " << port << endl;
+	int check_bind = bind(s, (struct sockaddr*) &sa, sas);
+	if (check_bind == -1) {
+		perror("Error while binding socket");
+		exit(1);
+	}
+
+	//bind(s, (struct sockaddr*) &sa, sas);
 
     char strg[300];
 
-    listen(s, 5);
+    cout << "Listening" << endl;
+	int check_listen = listen(s, 5);
+	if (check_listen == -1) {
+		perror("Error while listening on socket");
+		exit(1);
+	}
 
     struct sockaddr_in cla;
 
     int clas = sizeof(cla);
 
-    int cls = accept(s, (struct sockaddr*) &cla, (socklen_t*) &clas);
+    cout << "Accepting" << endl;
+	int cls = accept(s, (struct sockaddr*) &cla, (socklen_t*) &clas);
+	if (cls == -1) {
+		perror("Error while accepting");
+	}
 
     char hello[300] = "Hello!";
 
